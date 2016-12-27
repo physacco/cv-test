758d09c3ed15b612393ac3a4feae03264fc49487
physacco <physacco@gmail.com>
2016-12-27 14:39:14 +0800

    change build directory

--- tools/build_linux.sh
+++ tools/build_linux.sh
@@ -4,12 +4,12 @@ set -e
 
 SCRIPT_DIR=$(dirname "$0")
 PROJECT_DIR=$(cd "$SCRIPT_DIR"; cd ..; pwd)
-BUILD_DIR=build
+BUILD_DIR=build/linux
 
 cd "$PROJECT_DIR"
 mkdir -p $BUILD_DIR
 cd $BUILD_DIR
 
-cmake ..
+cmake ${PROJECT_DIR}
 
 make -j $(nproc)
--- tools/build_osx.sh
+++ tools/build_osx.sh
@@ -4,7 +4,7 @@ set -e
 
 SCRIPT_DIR=$(dirname "$0")
 PROJECT_DIR=$(cd "$SCRIPT_DIR"; cd ..; pwd)
-BUILD_DIR=build
+BUILD_DIR=build/osx
 
 cd "$PROJECT_DIR"
 mkdir -p $BUILD_DIR
@@ -12,6 +12,6 @@ cd $BUILD_DIR
 
 # use brew-installed opencv3
 export CMAKE_PREFIX_PATH=/usr/local/opt/opencv3
-cmake ..
+cmake ${PROJECT_DIR}
 
 make -j $(sysctl -n hw.ncpu)
