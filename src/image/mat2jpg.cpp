41def01fe3389307e0edaa8349765d32a72e720e
physacco <physacco@gmail.com>
2018-08-04 16:32:12 +0800

    use jpeg_mem_dest

--- src/image/mat2jpg.cpp
+++ src/image/mat2jpg.cpp
@@ -93,6 +93,27 @@ std::vector<uint8_t> read_file(const char* file_name) {
   return data;
 }
 
+void write_file(const char* file_name, const char* data, size_t size) {
+  FILE* f = fopen(file_name, "wb");
+  if (!f) {
+    fprintf(stderr, "Cannot open %s: %s\n", file_name, strerror(errno));
+    exit(EXIT_FAILURE);
+  }
+
+  size_t bytes_written = fwrite(data, 1, size, f);
+  if (bytes_written != size) {
+    fprintf(stderr, "incomplete write: %lu/%lu, error: %s\n", size, bytes_written,
+            strerror(errno));
+    exit(EXIT_FAILURE);
+  }
+
+  int status = fclose(f);
+  if (status != 0) {
+    fprintf(stderr, "Error closing '%s': %s.\n", file_name, strerror(errno));
+    exit(EXIT_FAILURE);
+  }
+}
+
 void bgr2rgb(char* data, int w, int h) {
   for (int i = 0; i < h; ++i) {
     for (int j = 0; j < w; ++j) {
@@ -150,6 +171,53 @@ void write_jpeg_file(const char* filename, char* data, int w, int h) {
   // release JPEG compression object
   // This is an important step since it will release a good deal of memory.
   jpeg_destroy_compress(&cinfo);
+}
+
+void write_jpeg_file2(const char* filename, char* data, int w, int h) {
+  JSAMPLE* image_buffer = reinterpret_cast<JSAMPLE*>(data);
+  struct jpeg_compress_struct cinfo;
+  struct jpeg_error_mgr jerr;
+  JSAMPROW row_pointer[1];  // pointer to JSAMPLE row[s]
+  int row_stride = w * 3;   // JSAMPLEs per row in image_buffer
+
+  cinfo.err = jpeg_std_error(&jerr);
+  jpeg_create_compress(&cinfo);
+
+  uint8_t* outbuffer = nullptr;  // must be 0
+  size_t outsize = 0;  // must be 0
+  jpeg_mem_dest(&cinfo, &outbuffer, &outsize);
+
+  cinfo.image_width = w;  // image width and height, in pixels
+  cinfo.image_height = h;
+  cinfo.input_components = 3;      // # of color components per pixel
+  cinfo.in_color_space = JCS_RGB;  // colorspace of input image
+
+  jpeg_set_defaults(&cinfo);
+  jpeg_set_quality(&cinfo, config.quality,
+                   TRUE);  // limit to baseline-JPEG values
+
+  jpeg_start_compress(&cinfo, TRUE);
+
+  while (cinfo.next_scanline < cinfo.image_height) {
+    // jpeg_write_scanlines expects an array of pointers to scanlines.
+    // Here the array is only one element long, but you could pass
+    // more than one scanline at a time if that's more convenient.
+    row_pointer[0] = &image_buffer[cinfo.next_scanline * row_stride];
+    (void)jpeg_write_scanlines(&cinfo, row_pointer, 1);
+  }
+
+  // Finish compression
+  jpeg_finish_compress(&cinfo);
+
+  // release JPEG compression object
+  // This is an important step since it will release a good deal of memory.
+  jpeg_destroy_compress(&cinfo);
+
+  // Write file
+  write_file(filename, (const char*)outbuffer, outsize);
+
+  // free the jpeg buffer
+  free(outbuffer);
 }
 
 void show_usage(FILE* stream) {
@@ -234,7 +302,7 @@ int main(int argc, char** argv) {
     bgr2rgb(data, config.width, config.height);
   }
 
-  write_jpeg_file(output_file, data, config.width, config.height);
+  write_jpeg_file2(output_file, data, config.width, config.height);
 
   return 0;
 }
